8ad9c6ba9c377f80e0c69e8886875a1b1d185c59
Sofia Kravtsova <sofia.kravtsova12@gmail.com>
2021-12-22 21:57:28 +0200

    initial commit

---
+++ Sofia_Kravtsova_DZ1.py
@@ -0,0 +1,10 @@
+# list
+покупки = ['молоко', 'хлеб', 'яйца', 'колбаса', 'сыр']
+print(покупки)
+покупки.append('мука')
+del покупки [3]
+print(покупки)
+
+#tuple
+weapon = ('sword', 'bow', 'gun', 'disc', 'axe')
+print(weapon[-1])
