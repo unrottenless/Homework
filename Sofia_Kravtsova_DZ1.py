ad215ac3448cd9d57d7eb43117ef8d8a4605beca
unrottenless <96542631+unrottenless@users.noreply.github.com>
2021-12-22 22:47:47 +0200

    Update Sofia_Kravtsova_DZ1.py

--- Sofia_Kravtsova_DZ1.py
+++ Sofia_Kravtsova_DZ1.py
@@ -8,3 +8,12 @@ print(покупки)
 #tuple
 weapon = ('sword', 'bow', 'gun', 'disc', 'axe')
 print(weapon[-1])
+
+#dict
+Mr = {'O':16.0, 'H':1.0, 'Cl':36.0, 'Na':23.0}
+Mr ['O'] = 35.5
+print(Mr)
+
+#set
+ноты = {'до', 'ре', 'ми', 'фа', 'соль', 'ля', 'си'}
+print(ноты)
